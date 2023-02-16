16f370f0283efaf9ff61cd7652ccc174f2b5b0bc
Max lindberg <112978463+Max-lindberg@users.noreply.github.com>
2023-02-16 18:27:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+import time
+
+
+
 print("                ▄▄▄▄  ▄▄   ▄▄                                                    ▄▄▄▄   ▄▄▄▄                ")
 print("▀███▀   ▀██▀  ▄█▀ ▀▀▀███   ██        ▀███                                      ▄█▀ ▀▀ ▄█▀ ▀▀")
 print("  ███▄  ▄█    ██▀     ██               ██                                      ██▀    ██▀")
@@ -9,7 +16,7 @@ print("▄██▄▄  ▄▄███▄████▄  ▄████▄█
 print("                                                           ██")
 print("                                                         ▄████▄")
 
-keylist = ["t9R#x8uKp$z%Y5", "7jL@vPmN&c2Q*H", "s6G$bDf3nE!wA4", "F8#yMhT%Zx9k1U", "qW@pXoRm7V*JlI", "e2C$dSjN&g5BtK", "6iA!kOwYf#G4xL", "b1U@2JyH9q3MvP", "V$rS0nT7zFt@Ll", "h5N#fXc9B!sWd8", "a4Z*mPwJ@u&yK7", "Q8@8HbU9gNt1Ei", "2rC#kLl3nWfV7I", "D$pRt@hXm6yGxK", "g1A&fYlN5zW!oB"]
+keylist = ["t9R#x8uKp$z%Y5", "7jL@vPmN&c2Q*H", "s6G$bDf3nE!wA4", "F8#yMhT%Zx9k1U", "qW@pXoRm7V*JlI", "e2C$dSjN&g5BtK", "6iA!kOwYf#G4xL", "b1U@2JyH9q3MvP", "V$rS0nT7zFt@Ll", "h5N#fXc9B!sWd8", "a4Z*mPwJ@u&yK7", "Q8@8HbU9gNt1Ei", "2rC#kLl3nWfV7I", "D$pRt@hXm6yGxK", "g1A&fYlN5zW!oB", "root"]
 
 inputkey = input("Enter your key/password: ")
 
@@ -20,15 +27,11 @@ else:
     print("Reload program and try again.")
     quit
 
+time.sleep(5)
 
-print("Now unlink your discord and make a new steam ac!")
-print("------------------------------------------------------------------------------------------------")
-print("change your fivem version to release")
-print("------------------------------------------------------------------------------------------------")
+print("Delelte Digital Intelements!")
 
-import os
-import tkinter as tk
-from tkinter import filedialog
+time.sleep(5)
 
 # Opret hovedvindue
 root = tk.Tk()
@@ -64,4 +67,58 @@ except FileNotFoundError:
 
 # Vis hovedvindue
 root.mainloop()
+
+print("Now unlink your discord and make a new steam ac!")
+print("------------------------------------------------------------------------------------------------")
+print("change your fivem version to release")
+print("------------------------------------------------------------------------------------------------")
+
+time.sleep(5)
+print("")
+print("")
+print("")
+print("")
+print("Delete Logs Folder!")
+print("------------------------------------------------------------------------------------------------")
+
+
+
+# Opret hovedvindue
+root = tk.Tk()
+
+# Funktion til at slette fil
+def delete_file():
+    selected_item = listbox.curselection()[0]  # Få den valgte fil fra listen
+    file_path = listbox.get(selected_item)  # Få filstien
+    os.remove(file_path)  # Slet filen
+    listbox.delete(selected_item)  # Fjern filen fra listen
+
+# Angiv stien til FiveM-mappen
+fivem_path = os.path.expanduser("~") + "/AppData/Local/FiveM"
+
+try:
+    # Skift til FiveM-mappen
+    os.chdir(fivem_path)
+
+    # Opret liste over filer
+    files = os.listdir()
+    listbox = tk.Listbox(root)
+    for file in files:
+        listbox.insert(tk.END, file)
+    listbox.pack()
+
+    # Opret knap til at slette fil
+    button_delete = tk.Button(root, text="Slet fil", command=delete_file)
+    button_delete.pack()
+
+except FileNotFoundError:
+    # Vis en fejlmeddelelse, hvis FiveM-mappen ikke kunne findes
+    tk.messagebox.showerror("Fejl", "Could not find your fivem...")
+
+# Vis hovedvindue
+root.mainloop()
+
+
+
+
 
